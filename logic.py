bebff481daa73e92211424d73869d8421cc976c2
NathanJ04 <njohnson108@unomaha.edu>
2024-05-07 23:53:45 -0500

    Setup of code

--- logic.py
+++ logic.py
@@ -6,3 +6,15 @@ class Logic(QMainWindow, Ui_Dialog):
         super().__init__()
         self.setupUi(self)
 
+        self.button_home.clicked.connect(lambda: self.home_page())
+        self.button_post_score.clicked.connect(lambda: self.post_page())
+        self.button_stats.clicked.connect(lambda: self.stats_page())
+
+    def home_page(self):
+        self.stacked_widget.setCurrentIndex(0)
+
+    def post_page(self):
+        self.stacked_widget.setCurrentIndex(1)
+
+    def stats_page(self):
+        self.stacked_widget.setCurrentIndex(2)
